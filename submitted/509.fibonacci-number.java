b1de50e7dac81fc65d24c1580f6c1229d5883623
Pierre <qiqinyuan@gmail.com>
2022-07-28 07:02:05 +0800

    更新509

---
+++ submitted/509.fibonacci-number.java
@@ -0,0 +1,57 @@
+/*
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+*/
+
+/*
+ * @lc app=leetcode id=509 lang=java
+ *
+ * [509] Fibonacci Number
+ */
+
+// @lc code=start
+
+
+//说明：
+//使用DP,也可以变量数组简化成3个变量。
+
+// 31/31 cases passed (0 ms)
+// Your runtime beats 100 % of java submissions
+// Your memory usage beats 94.19 % of java submissions (38.9 MB)
+
+class Solution {
+    public int fib(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int p = 0, q = 0, r = 1;
+        for (int i = 2; i <= n; ++i) {
+            p = q; 
+            q = r; 
+            r = p + q;
+        }
+        return r;
+    }
+}
+// @lc code=end

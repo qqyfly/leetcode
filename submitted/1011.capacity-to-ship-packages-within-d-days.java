77f8b6dc7abd286ce3d35fb14b5a11ea27f66086
Pierre QI <qiqinyuan@gmail.com>
2022-07-29 11:09:37 +0800

    更新1011

---
+++ submitted/1011.capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,126 @@
+/*
+ * @lc app=leetcode id=1011 lang=java
+ *
+ * [1011] Capacity To Ship Packages Within D Days
+ */
+
+ /*
+A conveyor belt has packages that must be shipped from one port to another within days days.
+
+The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor belt (in the order given by weights). We may not load more weight than the maximum weight capacity of the ship.
+
+Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days.
+
+ 
+
+Example 1:
+
+Input: weights = [1,2,3,4,5,6,7,8,9,10], days = 5
+Output: 15
+Explanation: A ship capacity of 15 is the minimum to ship all the packages in 5 days like this:
+1st day: 1, 2, 3, 4, 5
+2nd day: 6, 7
+3rd day: 8
+4th day: 9
+5th day: 10
+
+Note that the cargo must be shipped in the order given, so using a ship of capacity 14 and splitting the packages into parts like (2, 3, 4, 5), (1, 6, 7), (8), (9), (10) is not allowed.
+Example 2:
+
+Input: weights = [3,2,2,4,1,4], days = 3
+Output: 6
+Explanation: A ship capacity of 6 is the minimum to ship all the packages in 3 days like this:
+1st day: 3, 2
+2nd day: 2, 4
+3rd day: 1, 4
+Example 3:
+
+Input: weights = [1,2,3,1,1], days = 4
+Output: 3
+Explanation:
+1st day: 1
+2nd day: 2
+3rd day: 3
+4th day: 1, 1
+ 
+
+Constraints:
+
+1 <= days <= weights.length <= 5 * 104
+1 <= weights[i] <= 500
+  */
+// @lc code=start
+
+
+//BS
+// 85/85 cases passed (13 ms)
+// Your runtime beats 58.27 % of java submissions
+// Your memory usage beats 34.48 % of java submissions (54.3 MB)
+class Solution {
+    int[] weights;
+    public int shipWithinDays(int[] weights, int days) {
+        this.weights = weights;
+        int l = 1;
+        int r = Integer.MAX_VALUE;
+        int ans = 0;
+        while(l <= r){
+            int m = l + (r - l) / 2;
+            if (getDays(m) <= days){
+                r = m - 1;
+                ans = m;
+            }else{
+                l = m + 1;
+            }
+            
+        } 
+        return ans;
+    }
+
+    public int getDays(int capacity){        
+        int n = weights.length;
+        int days = 1;
+        int remaining = capacity;
+        int index = 0;
+        
+        while(index < n){
+            if (weights[index] <= remaining){
+                remaining -= weights[index];
+            }else{                
+                remaining = capacity - weights[index];                                
+                if (remaining < 0){
+                    return Integer.MAX_VALUE;
+                }
+                days ++;
+                
+            }
+            index ++;
+        }
+
+        return days;
+    }    
+}
+// @lc code=end
+
+
+class App {
+
+    public static void main(String[] args) {
+        long startTime=System.currentTimeMillis(); 
+        
+        //--------CODE 🡃------------
+        Solution sol = new Solution();
+        int[] weights = new int[]{500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500,500};
+        sol.shipWithinDays(weights,1);
+        
+        //--------CODE 🡁------------
+        long endTime=System.currentTimeMillis(); 
+        long totalTime = endTime - startTime;
+        if (totalTime >= 1000){
+            totalTime /= 1000;
+            System.out.println("Total time: " + totalTime + " Seconds");      
+        }else{
+            System.out.println("Total time: " + totalTime + " ms");      
+        }
+        
+    }
+}

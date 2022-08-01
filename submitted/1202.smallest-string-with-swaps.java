71c8df15497cd342ff2fde1ff45e018c608b9c8f
Pierre QI <qiqinyuan@gmail.com>
2022-08-01 16:45:53 +0800

    更新1202

---
+++ submitted/1202.smallest-string-with-swaps.java
@@ -0,0 +1,146 @@
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=1202 lang=java
+ *
+ * [1202] Smallest String With Swaps
+ */
+
+ /*
+You are given a string s, and an array of pairs of indices in the string pairs where pairs[i] = [a, b] indicates 2 indices(0-indexed) of the string.
+
+You can swap the characters at any pair of indices in the given pairs any number of times.
+
+Return the lexicographically smallest string that s can be changed to after using the swaps.
+
+ 
+
+Example 1:
+
+Input: s = "dcab", pairs = [[0,3],[1,2]]
+Output: "bacd"
+Explaination: 
+Swap s[0] and s[3], s = "bcad"
+Swap s[1] and s[2], s = "bacd"
+Example 2:
+
+Input: s = "dcab", pairs = [[0,3],[1,2],[0,2]]
+Output: "abcd"
+Explaination: 
+Swap s[0] and s[3], s = "bcad"
+Swap s[0] and s[2], s = "acbd"
+Swap s[1] and s[2], s = "abcd"
+Example 3:
+
+Input: s = "cba", pairs = [[0,1],[1,2]]
+Output: "abc"
+Explaination: 
+Swap s[0] and s[1], s = "bca"
+Swap s[1] and s[2], s = "bac"
+Swap s[0] and s[1], s = "abc"
+ 
+
+Constraints:
+
+1 <= s.length <= 10^5
+0 <= pairs.length <= 10^5
+0 <= pairs[i][0], pairs[i][1] < s.length
+s only contains lower case English letters.
+  */
+
+// @lc code=start
+
+//Disjoint 
+//UNION FIND 
+//算法很妙
+
+//首先UnionFind关联
+// 将祖先进hashmap,然后对应的value做成priorityQueue,自动排序
+//然后循环字符串，依次输出可以祖先列表中的数值，如果没有则原样返回
+
+
+// 36/36 cases passed (71 ms)
+// Your runtime beats 61.13 % of java submissions
+// Your memory usage beats 71.84 % of java submissions (98.7 MB)
+class Solution {
+    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+        if (pairs.size() == 0) {
+            return s;
+        }
+
+        // 第 1 步：将任意交换的结点对输入并查集
+        int len = s.length();
+        UnionFind unionFind = new UnionFind(len);
+        for (List<Integer> pair : pairs) {
+            int index1 = pair.get(0);
+            int index2 = pair.get(1);
+            unionFind.union(index1, index2);
+        }
+
+        // 第 2 步：构建映射关系
+        char[] charArray = s.toCharArray();
+        // key：连通分量的代表元，value：同一个连通分量的字符集合（保存在一个优先队列中）
+        Map<Integer, PriorityQueue<Character>> hashMap = new HashMap<>(len);
+        for (int i = 0; i < len; i++) {
+            int root = unionFind.find(i);
+            if (hashMap.containsKey(root)) {
+                hashMap.get(root).offer(charArray[i]);
+            } else {
+                PriorityQueue<Character> minHeap = new PriorityQueue<>();
+                minHeap.offer(charArray[i]);
+                hashMap.put(root, minHeap);
+            }
+            // JDK 1.8后支持
+            //hashMap.computeIfAbsent(root, key -> new PriorityQueue<>()).offer(charArray[i]);
+        }
+
+        // 第 3 步：重组字符串
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            int root = unionFind.find(i);
+            stringBuilder.append(hashMap.get(root).poll());
+        }
+        return stringBuilder.toString();
+    }
+
+    private class UnionFind {
+
+        private int[] parent;
+        private int[] rank;
+
+        public UnionFind(int n) {
+            this.parent = new int[n];
+            this.rank = new int[n];
+            for (int i = 0; i < n; i++) {
+                this.parent[i] = i;
+                this.rank[i] = 1;
+            }
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY) {
+                return;
+            }
+
+            if (rank[rootX] == rank[rootY]) {
+                parent[rootX] = rootY;                
+                rank[rootY]++;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;                
+            } else {                
+                parent[rootY] = rootX;
+            }
+        }
+
+        public int find(int x) {
+            if (x != parent[x]) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+    }
+}
+// @lc code=end
+

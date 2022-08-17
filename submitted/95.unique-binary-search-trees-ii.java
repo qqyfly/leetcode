74f7a01d46a51ad263563ea71b73fc0854b90c21
Pierre QI <qiqinyuan@gmail.com>
2022-08-17 13:03:40 +0800

    95

---
+++ submitted/95.unique-binary-search-trees-ii.java
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode id=95 lang=java
+ *
+ * [95] Unique Binary Search Trees II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ 
+//  8/8 cases passed (2 ms)
+//  Your runtime beats 81.1 % of java submissions
+//  Your memory usage beats 56.47 % of java submissions (46.2 MB)
+// 递归,选择i中的一个作为跟节点,然后算出所有左树,右树,然后两个for加到答案即可
+
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        List<TreeNode> ans = new ArrayList<TreeNode>();
+        if (n == 0) {
+            return ans;
+        }
+        return getAns(1, n);
+    }
+
+    private List<TreeNode> getAns(int start, int end) { 
+        List<TreeNode> ans = new ArrayList<TreeNode>();
+        //此时没有数字，将 null 加入结果中
+        if (start > end) {
+            ans.add(null);
+            return ans;
+        }
+        //只有一个数字，当前数字作为一棵树加入结果中
+        if (start == end) {
+            TreeNode tree = new TreeNode(start);
+            ans.add(tree);
+            return ans;
+        }
+        //尝试每个数字作为根节点
+        for (int i = start; i <= end; i++) {
+            //得到所有可能的左子树
+            List<TreeNode> leftTrees = getAns(start, i - 1);
+             //得到所有可能的右子树
+            List<TreeNode> rightTrees = getAns(i + 1, end);
+            //左子树右子树两两组合
+            for (TreeNode leftTree : leftTrees) {
+                for (TreeNode rightTree : rightTrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = leftTree;
+                    root.right = rightTree;
+                    //加入到最终结果中
+                    ans.add(root);
+                }
+            }
+        }
+        return ans;
+    }
+}
+// @lc code=end
+
